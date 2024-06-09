f58a37f14dcfa449a0c0a6a5aa9033978a55db6c
codingkiddo <codingkiddo@gmail.com>
2024-06-09 15:10:28 +0530

    chapter 3

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,30 +1,15 @@
 package com.optimagrowth.license;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import com.optimagrowth.license.service.client.UserContextInterceptor;
-
 @SpringBootApplication
-@RefreshScope
-@EnableDiscoveryClient
-@EnableFeignClients
-@EnableEurekaClient
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
@@ -43,20 +28,6 @@ public class LicenseServiceApplication {
 		messageSource.setUseCodeAsDefaultMessage(true);
 		messageSource.setBasenames("messages");
 		return messageSource;
-	}
-
-	@LoadBalanced
-	@Bean
-	public RestTemplate getRestTemplate(){
-		RestTemplate restTemplate = new RestTemplate();
-		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
-		if ( interceptors == null ) {
-			restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-		} else {
-			interceptors.add(new UserContextInterceptor());
-			restTemplate.setInterceptors(interceptors);
-		}
-		return restTemplate;
 	}
 
 }
--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,69 +3,73 @@ package com.optimagrowth.license.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
-import java.util.concurrent.TimeoutException;
+import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
-import com.optimagrowth.license.utils.UserContextHolder;
 
 @RestController
 @RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(LicenseController.class);
 
 	@Autowired
 	private LicenseService licenseService;
 
-	@GetMapping("/{licenseId}")
+	@RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
 	public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
 			@PathVariable("licenseId") String licenseId) {
-
-		License license = licenseService.getLicense(licenseId, organizationId, "");
+		
+		License license = licenseService.getLicense(licenseId, organizationId);
 		license.add( 
 				linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
-				linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
-				linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
-				linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
-				);
-
+				linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null)).withRel("createLicense"),
+				linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
+				linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+		);
+		
 		return ResponseEntity.ok(license);
 	}
 
-	@GetMapping("/{licenseId}/{clientType}")
-	public License getLicensesWithClient( @PathVariable("organizationId") String organizationId,
-			@PathVariable("licenseId") String licenseId,
-			@PathVariable("clientType") String clientType) {
-
-		return licenseService.getLicense(licenseId, organizationId, clientType);
+	@RequestMapping(value="/",method = RequestMethod.GET)
+	public ResponseEntity<License> getAllLicense( @PathVariable("organizationId") String organizationId,
+			@PathVariable("licenseId") String licenseId) {
+		
+		License license = licenseService.getLicense(licenseId, organizationId);
+		license.add( 
+				linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+				linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null)).withRel("createLicense"),
+				linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
+				linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+		);
+		
+		return ResponseEntity.ok(license);
 	}
-
+	
 	@PutMapping
-	public ResponseEntity<License> updateLicense(@RequestBody License request) {
-		return ResponseEntity.ok(licenseService.updateLicense(request));
+	public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+		return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
 	}
 
 	@PostMapping
-	public ResponseEntity<License> createLicense(@RequestBody License request) {
-		return ResponseEntity.ok(licenseService.createLicense(request));
+	public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request,
+			@RequestHeader(value = "Accept-Language",required = false) Locale locale) {
+		return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
 	}
 
 	@DeleteMapping(value="/{licenseId}")
-	public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
-		return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+	public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+		return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
 	}
-
-	@GetMapping("/")
-	public List<License> getLicenses( @PathVariable("organizationId") String organizationId) throws TimeoutException {
-		logger.debug("LicenseServiceController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
-		return licenseService.getLicensesByOrganization(organizationId);
-	}
-
 }
